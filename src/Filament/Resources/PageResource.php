978dffc506dcc488a44afcf01cd688db34274aaa
Steen Rabol <steen@rabol.com>
2024-09-29 19:20:42 +0200

    Change Text from Post to Page

--- src/Filament/Resources/PageResource.php
+++ src/Filament/Resources/PageResource.php
@@ -65,7 +65,7 @@ class PageResource extends SkyResource
             Tabs::make('post_tabs')->schema([
                 Tabs\Tab::make(__('Title & Content'))->schema([
                     TextInput::make('title')
-                        ->label(__('Post Title'))
+                        ->label(__('Page Title'))
                         ->required()
                         ->maxLength(255)
                         ->live(onBlur: true)
@@ -86,13 +86,13 @@ class PageResource extends SkyResource
                     Textarea::make('description')
                         ->maxLength(255)
                         ->label(__('Description'))
-                        ->hint(__('Write an excerpt for your post')),
+                        ->hint(__('Write an excerpt for your page')),
 
                     TextInput::make('slug')
                         ->unique(ignorable: fn (?Post $record): ?Post => $record)
                         ->required()
                         ->maxLength(255)
-                        ->label(__('Post Slug')),
+                        ->label(__('Page Slug')),
 
                     Select::make('parent_id')
                         ->options(SkyPlugin::get()->getModel('Post')::where('post_type', 'page')->pluck(
